7000eabd262a6e184972385ba51fd1a3df684321
Ivan Chau <ivanmchau@gmail.com>
2015-11-05 16:41:53 -0500

    Niva, I outlined some stuff for you to read; :D

--- recontrer/CalendarControllerViewController.swift
+++ recontrer/CalendarControllerViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 import Foundation
-import CVCalendar
+import CVCalendar //Importing Calendar Module
+//Delegates allow the ViewController to read and change special classes (UITables, CVCalendars, etc.)
 class CalendarControllerViewController: UIViewController, UITableViewDataSource, CVCalendarViewDelegate, CVCalendarMenuViewDelegate {
+    //global variables, representing parts in the View :)
     @IBOutlet weak var meetings : UILabel?
     @IBOutlet weak var tableView : UITableView!
     @IBOutlet weak var calendarView : CVCalendarView!
     @IBOutlet weak var calendarMenuView : CVCalendarMenuView!
    
+    //Gmail initialization.
     private let kKeychainItemName = "recontrer: Gmail API"
     private let kClientID = "637818622405-uddaldcou24vuk3jgsngna1fhs2vapmj.apps.googleusercontent.com"
     private let kClientSecret = "rzdqWU46tNGuNk91Tc70SKln"
@@ -24,18 +27,24 @@ class CalendarControllerViewController: UIViewController, UITableViewDataSource,
     private let service = GTLServiceGmail()
     var messagesCount = 0
     var doneCount = 0
+    //MessagesArray is the messages returned from the Gmail API. Eventually we want to save these and only get new ones.
     var messagesArray = NSMutableArray()
+    //tableArray is the array of messages that are deemed 'meetings'.
     var tableArray = NSMutableArray()
     // When the view loads, create necessary subviews
     // and initialize the Gmail API service
+    
     override func viewDidLoad() {
+        //viewDidLoad is a void function that is always called first automatically.
         super.viewDidLoad()
         self.tableView!.dataSource = self;
+        //setting the tableView Delegate to the controller
         self.service.authorizer = GTMOAuth2ViewControllerTouch.authForGoogleFromKeychainForName(
             kKeychainItemName,
             clientID: kClientID,
             clientSecret: kClientSecret
         )
+        //setting up security gmail api
         
     }
     
@@ -52,6 +61,7 @@ class CalendarControllerViewController: UIViewController, UITableViewDataSource,
                 completion: nil
             )
         }
+        
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -59,6 +69,7 @@ class CalendarControllerViewController: UIViewController, UITableViewDataSource,
         self.calendarView!.commitCalendarViewUpdate()
         self.calendarMenuView!.commitMenuViewUpdate()
         NSLog("viewLayedOut")
+        //this is when the view lays out its views (the calendar needs this to run properly, otherwise it would be difficult to see)
     }
     func presentationMode() -> CalendarMode{
         NSLog("Presentation Set")
@@ -71,6 +82,7 @@ class CalendarControllerViewController: UIViewController, UITableViewDataSource,
     func dayOfWeekTextColor() -> UIColor {
         return UIColor.whiteColor()
     }
+    //above, setting up the basic requirements of being a Calendar Delegate -> you have to fulfill a couple of required functions to set up the Calendar.
     
     // Construct a query and get a list of upcoming labels from the gmail API
     func fetchMessages() {
@@ -80,13 +92,14 @@ class CalendarControllerViewController: UIViewController, UITableViewDataSource,
             didFinishSelector : "logResultWithTicket:finishedWithObject:error:")
         
     }
-    
+    //Fetching emails from Gmail API ^ with query to server (this only returns object IDS)
     func logResultWithTicket(ticket : GTLServiceTicket,
         finishedWithObject messResponse : GTLGmailListMessagesResponse,
         error : NSError?) {
             if let error = error {
                 showAlert("Error", message: error.localizedDescription)
             }
+            //if there is no error, read what the server returns. Now, get the contents of every message.
             if !messResponse.messages.isEmpty {
                 for message in messResponse.messages as! [GTLGmailMessage]{
                     messagesCount = messResponse.messages.count
@@ -106,19 +119,22 @@ class CalendarControllerViewController: UIViewController, UITableViewDataSource,
             showAlert("Error 2", message: error.localizedDescription)
             return
         } else {
+            //for every message content returned, dissect it.
             parseMailInfo(eachMessageResponse)
         }
     }
     
     func parseMailInfo(mailInfo : GTLGmailMessage){
+        //dissect email
         doneCount++
         let labelId = mailInfo.labelIds[0] as! String
         if labelId == "INBOX" {
+            //mailInformationDictionary is a dictionary : it's basically an array of key values. Example : {key : value, key1 : value1}
             let mailInformationDict = NSMutableDictionary()
             mailInformationDict.setValue(mailInfo.snippet as String, forKey: "snippet")
             mailInformationDict.setValue(mailInfo.identifier as String, forKey: "gmailId")
             mailInformationDict.setValue(mailInfo.internalDate, forKey: "internaldate")
-            
+            //setting basic info about the message into the dictionary
             let payloadArray = mailInfo.payload.headers as NSArray
             
             for obj in payloadArray {
@@ -152,6 +168,7 @@ class CalendarControllerViewController: UIViewController, UITableViewDataSource,
                     
                     mailInformationDict.setValue(substringTo, forKey: "to")
                 }
+                //getting subject, from, and to from the server. It involves some unwrapping. Setting it as another value in dictionary.
             }
             var parts = mailInfo.payload.parts
             var decodedBody: NSString?
@@ -166,13 +183,18 @@ class CalendarControllerViewController: UIViewController, UITableViewDataSource,
                     decodedBody = NSString(data: decodedData!, encoding: NSUTF8StringEncoding)
                     mailInformationDict.setValue(decodedBody, forKey: "body")
                 }
+                //getting to the actual body of the email (the text part that you're supposed to read), and decoding it.
                 
             }
             messagesArray.addObject(mailInformationDict)
+            //adding the mailInformationDictionary to the messagesArray as one of the messages We've Received. 
+            //This piece of code will repeat for every message returned, as the function will be called more than once.
         }
         if messagesCount == doneCount{
             doneCount = 0
             self.findMeetings(messagesArray)
+            //if the messages we pulled from the server is the same as the number that passes through the function, stop
+            //now we need to find the emails that are meetings.
         }
         
         
@@ -190,6 +212,7 @@ class CalendarControllerViewController: UIViewController, UITableViewDataSource,
             delegate: self,
             finishedSelector: "viewController:finishedWithAuth:error:"
         )
+        //if you haven't signed in, this will generate a view for you to type in credentials.
     }
     
     // Handle completion of the authorization process, and update the Gmail API
@@ -205,6 +228,7 @@ class CalendarControllerViewController: UIViewController, UITableViewDataSource,
             
             service.authorizer = authResult
             dismissViewControllerAnimated(true, completion: nil)
+            //credential views!
     }
     
     // Helper for showing an alert
@@ -238,11 +262,12 @@ class CalendarControllerViewController: UIViewController, UITableViewDataSource,
                     tame.setObject(dictionary.objectForKey("from")!, forKey:"from")
                     tame.setObject(date.description, forKey: "date")
                     self.tableArray.addObject(tame)
-                    
+                    // going in to the body and checking if there is the word 'tomorrow' and 'meeting' in the body, taking a part of it, and making another dictionary. Add it to the tableArray!
                 }
             }
         }
         self.tableView.reloadData()
+        //this will allow the tableView to now take the data we just got and put it into cells.
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell:MeetingTableViewCell = self.tableView.dequeueReusableCellWithIdentifier("MeetCell") as! MeetingTableViewCell
@@ -253,11 +278,12 @@ class CalendarControllerViewController: UIViewController, UITableViewDataSource,
         cell.from?.text = deal.valueForKey("from") as? String
         cell.dateSent?.text = deal.valueForKey("date") as? String
         cell.snippet?.text = "..." + (deal.valueForKey("snippet") as? String)! + "..."
-        
+        //for each object in the dissected emails array, take each of the data and put it in a cell.
         return cell
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tableArray.count
+        //for each object in the array, make it a row in the tableView
     }
     
     override func didReceiveMemoryWarning() {
